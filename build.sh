8bb707841b9afec659facdb0053b378db398f003
Tomasz Worsztynowicz <tomasz.worsztynowicz@siili.com>
2017-07-04 15:01:20 +0200

    standalone version with fixed ips

--- build.sh
+++ build.sh
@@ -5,6 +5,6 @@ AWS_OWNER="921100211113"
 sbt assembly
 docker build -t akka-cluster-poc .
 
-docker tag akka-cluster-poc:latest ${AWS_OWNER}.dkr.ecr.eu-west-2.amazonaws.com/akka-cluster-poc:latest
-$(aws ecr get-login --region eu-west-2)
-docker push ${AWS_OWNER}.dkr.ecr.eu-west-2.amazonaws.com/akka-cluster-poc:latest
+#docker tag akka-cluster-poc:latest ${AWS_OWNER}.dkr.ecr.eu-west-2.amazonaws.com/akka-cluster-poc:latest
+#$(aws ecr get-login --region eu-west-2)
+#docker push ${AWS_OWNER}.dkr.ecr.eu-west-2.amazonaws.com/akka-cluster-poc:latest
--- src/main/scala/AppConfiguration.scala
+++ src/main/scala/AppConfiguration.scala
@@ -1,41 +1,20 @@
 import java.net.InetAddress
 
 import com.typesafe.config.{ConfigFactory, ConfigValueFactory}
-import scala.collection.JavaConverters._
-import AWSInstanceConfiguration._
 
 /**
   * Created on 30/06/17.
   */
 object AppConfiguration {
 
-  private lazy val originConfig = ConfigFactory.load()
+  lazy val config = ConfigFactory.empty()
+    .withValue("akka.remote.netty.tcp.bind-hostname",
+      ConfigValueFactory.fromAnyRef(InetAddress.getLocalHost.getHostAddress))
+    .withFallback(ConfigFactory.load())
 
-  lazy val clusterName = originConfig.getString("akka-cluster-poc.clustering.cluster.name")
-  lazy val httpPort = originConfig.getInt("akka-cluster-poc.http.port")
-  private lazy val clusterSeedPort = originConfig.getString("akka-cluster-poc.clustering.seed-port")
-  private lazy val awsClusterName = originConfig.getString("akka-cluster-poc.aws.cluster.name")
+  lazy val clusterName = config.getString("akka-cluster-poc.clustering.cluster.name")
+  lazy val httpPort = config.getInt("akka-cluster-poc.http.port")
+  private lazy val clusterSeedPort = config.getString("akka-cluster-poc.clustering.seed-port")
+  private lazy val awsClusterName = config.getString("akka-cluster-poc.aws.cluster.name")
 
-  lazy val config = {
-
-    val myIp = AWSInstanceConfiguration.selfEc2InstanceIp.get
-    val myLocalIp = InetAddress.getLocalHost.getHostAddress
-    val natConfig = ConfigFactory.empty()
-      .withValue("akka-cluster-poc.clustering.hostname", ConfigValueFactory.fromAnyRef(myIp))
-      .withValue("akka-cluster-poc.clustering.bind-hostname", ConfigValueFactory.fromAnyRef(myLocalIp))
-      .withValue("akka.remote.netty.tcp.hostname", ConfigValueFactory.fromAnyRef(myIp))
-      .withValue("akka.remote.netty.tcp.bind-hostname", ConfigValueFactory.fromAnyRef(myLocalIp))
-
-    AWSInstanceConfiguration.ec2InstanceIps(awsClusterName) match {
-      case Left(_) => natConfig.withFallback(originConfig.withFallback(originConfig))
-      case Right(ips) => { natConfig.withValue("akka.cluster.seed-nodes",
-        ConfigValueFactory.fromIterable(
-          ips.map(ip => s"akka.tcp://$clusterName@$ip:$clusterSeedPort")
-            .asJava))
-        .withFallback(originConfig)
-      }
-    }
-
-
-  }
 }
